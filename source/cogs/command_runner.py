d9884cf110fba6d858d4ad0a3bf00c42483037af
Mehmet Ozaslan <1mehmetaliozaslan@gmail.com>
2023-12-17 15:44:34 -0700

    Added pid command to terminate bot remotely

--- source/cogs/command_runner.py
+++ source/cogs/command_runner.py
@@ -1,13 +1,67 @@
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
+import os
+import asyncio
 import subprocess
+import tempfile
+import psutil
 
-class CommandRunner(commands.Cog): # create a class for our cog that inherits from commands.Cog
-    # this class is used to create a cog, which is a module that can be added to the bot
+from PIL import Image, ImageDraw, ImageFont
 
-    def __init__(self, bot): # this is a special method that is called when the cog is loaded
+
+class CommandRunner(commands.Cog): 
+    def __init__(self, bot):
         self.bot = bot
+
+    async def display_text_image(self, ctx, embed, msg):
+        max_width = 600
+        max_height = 800
+        font_size = 10
+
+        height = len(msg.split('\n')) * int(font_size*1.5)
+        width = max(len(line) for line in msg.split('\n')) * int(font_size*0.75)
+
+        img = Image.new('RGB', (width, height), color=(0, 0, 0))
+        img_file = tempfile.gettempdir() + '/output.png'
+        d = ImageDraw.Draw(img)
+
+        # Use a monospace font
+        font = ImageFont.truetype("DejaVuSansMono.ttf", 10)
+
+        # Draw the text on the image
+        d.multiline_text((10,10), msg, fill=(255,255,255), font=font,embedded_color=True)
+
+        # Save the image
+        img.save(img_file)        
+        file = discord.File(img_file, filename="output.png")
+        embed.set_image(url="attachment://output.png")
+
+        await ctx.send(file=file, embed=embed)
+
+
+    @discord.slash_command()
+    async def pid(self, ctx):
+        load_dotenv("../credentials/credentials.env")
+        discord_key = int(os.getenv("DISCORD_AUTHOR_ID"))
+
+        if ctx.author.id == discord_key:
+            # Get current process ID and parent process ID
+            pid = os.getpid()
+            ppid = os.getppid()
+
+            # Create an embed to display the information
+            embed = discord.Embed(
+                title="Process Information",
+                color=discord.Color.blue()
+            )
+            embed.add_field(name="PID", value=str(pid) + " " +  psutil.Process(pid).name(), inline=False)
+            embed.add_field(name="PPID", value=str(ppid) + " " + psutil.Process(ppid).name(), inline=False)
+
+            await ctx.respond(embed=embed)
+        else:
+            await ctx.respond("You do not have permission to use this command.")
+
 
     @discord.slash_command() # we can also add application commands
     async def run(self, ctx, *, msg):
@@ -16,34 +70,54 @@ class CommandRunner(commands.Cog): # create a class for our cog that inherits fr
         discord_key = int(os.getenv("DISCORD_AUTHOR_ID"))
         
         if ctx.author.id == discord_key:
-            # Display the user embed
-            embed = discord.Embed(title=f">{msg}", color=0x41fc03)  # You can change the color
-            await ctx.send(embed=embed)
+
+            await ctx.send(embed=discord.Embed(title="", color=0x41fc03).add_field(name="", value=f"```{msg[:1024]}```", inline=False))
 
             async with ctx.typing():
-                result = subprocess.run(msg, capture_output=True, text=True,shell=True)
+                process = await asyncio.create_subprocess_shell(
+                    msg,
+                    stdout=asyncio.subprocess.PIPE,
+                    stderr=asyncio.subprocess.PIPE
+                )
+                stdout, stderr = await process.communicate()
 
-                if result.stderr != "":
-                    truncated = ""
-                    if len(result.stderr) > 1024:
-                        truncated = "(truncated)"
+                if stderr:
+                    stderr = stderr.decode()
+                    if len (stderr) < 1024:
+                        error_embed = discord.Embed(
+                            title=f"Error: ({process.returncode})",
+                            color=0xFF0000
+                        )
+                        error_embed.add_field(name="", value=f"```{stderr}```", inline=False)
+                        await ctx.send(embed=error_embed)
 
-                    error_embed = discord.Embed(title=f"Error: ({result.returncode}) {truncated}", color=0xFF0000)  # You can change the color
-                    error_embed.add_field(name="", value=f"```{result.stderr[0:1024]}```", inline=False)
-                    await ctx.send(embed=error_embed)
+                    else:
+                        output_embed = discord.Embed(
+                            title=f"Error: ({process.returncode}) (truncated to image)",
+                            color=0xFF0000
+                        )
+                        await self.display_text_image(output_embed, stderr)
 
-                if result.stdout != "":
-                    truncated = ""
-                    if len(result.stdout) > 1024:
-                        truncated = "(truncated)"
 
-                    output_embed = discord.Embed(title=f"Output {truncated}:", color=0x41fc03)  # You can change the color
-                    output_embed.add_field(name="", value=f"```{result.stdout[0:1024]}```", inline=False)
-                    await ctx.send(embed=output_embed)
+                if stdout:
+                    stdout = stdout.decode()
+                    if len(stdout) < 1024:
+                        output_embed = discord.Embed(
+                            title="Output:",
+                            color=0x41fc03
+                        )
+                        output_embed.add_field(name="", value=f"```{stdout}```", inline=False)
+                        await ctx.send(embed=output_embed)
 
-                if result.stdout == "" and result.returncode:
-                    output_embed = discord.Embed(title=f"Success: {result.returncode}: ", color=0x41fc03)  # You can change the color
-                    
+                    else:
+                        output_embed = discord.Embed(
+                            title="Output" + (" (truncated to image):" if len(stdout) > 1024 else ":"),
+                            color=0x41fc03
+                        )
+                        await self.display_text_image(ctx, output_embed, stdout)
+
+                if not stdout and process.returncode == 0:
+                    await ctx.send(embed=discord.Embed(title="Success", color=0x41fc03))                    
 
 
         else:
